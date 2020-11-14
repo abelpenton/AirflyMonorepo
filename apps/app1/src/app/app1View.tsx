036843ce660356a173efbc28ff5b43687470970b
abelpenton <2235penton@gmail.com>
2020-11-14 00:28:04 -0500

    add example of how use the shared state in apps

--- apps/app1/src/app/app1View.tsx
+++ apps/app1/src/app/app1View.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+import {AirflyContext} from '@airfly-company/airfly-provider';
 
 export const App1View = () => {
+  const {state} = useContext(AirflyContext);
   return (
-    <div className="app">
+    <div style={{color: state.theme.color.background}} className="app">
         <h1>Welcome App1</h1>
     </div>
   );
